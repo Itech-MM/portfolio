e95033fdb59bbb81f32ad172673d0d909477a5ef
Sai Zaw Myint <96133665+SaiZawMyint@users.noreply.github.com>
2025-05-25 21:03:11 +0630

    Initial commit

---
+++ src/test/java/com/flexitech/app/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexitech.app.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
